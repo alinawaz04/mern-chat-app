52f3cfeb45a60b29fba75024ec8bf225fe70df2d
alinawaz04 <alihnawaz@gmail.com>
2024-07-28 20:49:53 -0500

    mongodb setup and define user schema/model

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,27 @@
 import express from "express";
 import { config } from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const PORT = process.env.PORT || 8000;
+// init express application
+const app = express();
 
 // loads .env file contents
 config();
 
-const PORT = process.env.PORT || 8000;
-
-// init express application
-const app = express();
-
-app.get("/", (req, res) => {
-  // route route (http://localhost:8000/)
-  res.send("Server ready");
-});
+app.use(express.json()); // parse incoming requests with JSON payloads (from req.body)
 
 // middleware for signup, login, logout routes
 app.use("/api/auth", authRoutes);
 
+// app.get("/", (req, res) => {
+//   // route route (http://localhost:8000/)
+//   res.send("Server ready");
+// });
+
 // listens for connections
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server running on port ${PORT}`);
+});
