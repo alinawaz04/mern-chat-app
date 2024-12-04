3e0bfdaea4833d98d44322d1672b197dac773ce0
alinawaz04 <alihnawaz@gmail.com>
2024-12-03 21:58:39 -0600

    sign up endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,43 @@
+import User from "../models/user.model.js";
+
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
-  } catch (error) {}
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords do not match" });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: "Passwords do not match" });
+    }
+
+    // TODO Hash password
+
+    const maleProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const femaleProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password,
+      gender,
+      profilePic: gender === "male" ? maleProfilePic : femaleProfilePic,
+    });
+
+    await newUser.save();
+    return res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      username: newUser.username,
+      profilePic: newUser.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    return res.status(400).json({ error: "Passwords do not match" });
+  }
 };
 
 export const login = (req, res) => {
