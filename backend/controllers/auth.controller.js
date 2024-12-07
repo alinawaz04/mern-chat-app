fee31e61d2ba264873527ddf2a8d2b18ef2f122b
alinawaz04 <alihnawaz@gmail.com>
2024-12-06 21:30:00 -0600

    login endpoint :)

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -47,12 +47,35 @@ export const signup = async (req, res) => {
     }
   } catch (error) {
     console.log("Error in signup controller", error.message);
-    return res.status(400).json({ error: "Passwords do not match" });
+    return res.status(400).json({ error: "Internal Server Error" });
   }
 };
 
-export const login = (req, res) => {
-  res.send("login route");
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    return res.status(400).json({ error: "Internal Server Error" });
+  }
 };
 
 export const logout = (req, res) => {
