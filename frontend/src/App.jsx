82b2a6a107b3a95c4a30d328a3d4d8fa63771e98
alinawaz04 <alihnawaz@gmail.com>
2025-04-08 14:24:23 -0500

    implement protected routes and authentication checks for login and signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,39 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import { useAuthContext } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
+  const { authUser } = useAuthContext();
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              {/* <ChatProvider> */}
+              <Home />
+              {/* </ChatProvider> */}
+            </ProtectedRoute>
+          }
+        />
+        {/* Public routes - redirect to home if already authenticated */}
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUp />}
+        />
+
+        {/* Catch-all route - redirect to home or login */}
+        <Route
+          path="*"
+          element={authUser ? <Navigate to="/" /> : <Navigate to="/login" />}
+        />
       </Routes>
     </div>
   );
